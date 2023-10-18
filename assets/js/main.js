d0936c5a81aa8e8b2413d6199dd6c5f6573c9dcb
neeraj-gs <gsneeraj05@gmail.com>
2023-10-18 21:25:48 +0530

    Added the fields seelction and connected emailjs and the form to amek it better

--- assets/js/main.js
+++ assets/js/main.js
@@ -41,6 +41,23 @@ const sendEmail = (e) => {
 
    //serviceId -template - #form - publicKey
    emailjs.sendForm('service_igdz0kb','template_d0mcsja','#contact-form','8p5Rtz2gkLnCzzsTM')
+   .then(()=>{
+      //show sent message
+// This code snippet adds functionality to a website's navigation menu. It allows the menu to be shown or hidden when the toggle button is clicked, and it also removes the menu when a link is clicked. Additionally, it adds a shadow to the header when
+      contactMessage.textContent = 'Message Sent Successfully '
+
+      //remving msg after 5 sec
+      setTimeout(()=>{
+         contactMessage.textContent = ''
+      },5000)
+
+
+      //clear input fields
+      contactForm.reset();
+   },()=>{
+      //show error messafe
+      contactMessage.textContent = 'Message Not Sent (Service Error) , Contact using Social links'
+   })
 }
 
 contactForm.addEventListener('submit',sendEmail)
